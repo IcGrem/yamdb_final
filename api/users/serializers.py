d7fa483631a89375f07b12766695f4da649cee76
Sergei Petukhov <Sergei.Petukhov@gmail.com>
2020-10-03 14:17:59 +0300

    47 line in users/ser

--- api/users/serializers.py
+++ api/users/serializers.py
@@ -41,10 +41,9 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError(
                 {'email': 'Email addresses must be unique.'}
             )
-        confirmation_code = encode(
-            ''.join(random.choice(
+        confirmation_code = encode(''.join(random.choice(
                 string.ascii_uppercase + string.digits
-                ) for _ in range(8)))
+        ) for _ in range(8)))
         username = email.replace('@', '_').replace('.', '_')
         email = email
         c_c = confirmation_code
